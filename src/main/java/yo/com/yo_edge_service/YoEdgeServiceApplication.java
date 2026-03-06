b86013bc76aa93cb73a4ffac679d8ae8185dcca1
MikeDePacina <mikedpacina@gmail.com>
2026-03-06 20:11:57 +0800

    first commit

---
+++ src/main/java/yo/com/yo_edge_service/YoEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package yo.com.yo_edge_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoEdgeServiceApplication.class, args);
+	}
+
+}
